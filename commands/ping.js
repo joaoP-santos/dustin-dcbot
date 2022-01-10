8c4c6eefd3c6a2047c6d4a88201fe9b56f103dd9
João Pedro Santos <potatohunter47@gmail.com>
2022-01-10 14:14:04 -0300

    made ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,22 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with "Pong"!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
+        .setDescription('Replies with the bot ping!'),
+    async execute(interaction, client) {
+        const embed = {
+            title: "Pong! :ping_pong:",
+            description: `Ping: ${Date.now() - interaction.createdTimestamp}`,
+            author: {
+                name: interaction.user.tag,
+                iconURL: interaction.user.avatarURL()
+            },
+            color: '#aaf542',
+            footer: {
+                text: `${module.exports.data.name} • ${module.exports.data.description}`,
+                iconURL: client.user.avatarURL()
+            }
+        }
+
+        interaction.reply({embeds: [embed]})
     },
 };
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('interactionCreate', async (interaction) => {
 
     //Runs the command
     try {
-        await command.execute(interaction);
+        await command.execute(interaction, client);
     } catch (error) {
         console.error(error);
         await interaction.reply({
